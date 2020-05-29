0e51cbbf47f8a7b6029815863bd6f373c24dc357
kumail <zdsd0019@gmail.com>
2020-05-29 14:44:44 -0400

    made the header

---
+++ src/component/header/header.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './header.scss'
+
+class header extends Component {
+    render() {
+        return (
+            <>
+            <header className='header'>
+                <h1 className='robson'>ROBSON</h1>
+                <span className='Rectangle-1'>
+                <h2>DINE SAFE</h2>
+                </span>
+                <input type='text' name='search' className='search' placeholder='Search'/> '
+            </header>
+            </>
+        );
+    }
+}
+
+export default header;
