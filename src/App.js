6babbd27d979a938b9f7db3c776ac3e0bc176c5b
kumail <zdsd0019@gmail.com>
2020-05-29 17:17:31 -0400

    bleh bleh

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import './component/header/header.scss';
 import Home from './pages/homePage'
 import Page2 from './pages/page2';
+import './pages/page2.scss'
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
