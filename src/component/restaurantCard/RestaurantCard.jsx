68d79d63bc9451ac084ba73548e3aebfd00f398e
Reece Simpson <reeceds147@gmail.com>
2020-05-29 15:19:00 -0400

    Created restautrant card

---
+++ src/component/restaurantCard/RestaurantCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const RestaurantCard = () => {
+
+    return (
+
+        <section className="Rectangle-10">
+            <div className="Vector-3"></div>
+            <div className="restaurant-card-info__container">
+                <p className="Longtime-eatery-offering-upscale-Italian-dishes-h">Longtime eatery offering upscale Italian dishes, handmade pastas & choice wines in a warm room.
+</p>
+                <div className="restaurant-card__button-container">
+                    <button className="Rectangle-14"><span className="menu">Menu</span></button>
+                    <button className="Rectangle-15"><span className="Book">Book</span></button>
+                </div>
+            </div>
+
+        </section>
+
+    )
+
+};
+
+export default RestaurantCard
